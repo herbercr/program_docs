ca7761224cadc4c7eac7b1806bdf06c1f10f1130
student <student@info-15.cshl.edu>
2022-10-11 16:35:29 -0400

    self explanatory

---
+++ python/if_else.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import math
+import sys
+
+#number system assignments
+num=int(sys.argv[1])
+ 
+#number assignments
+a=3
+b=3
+c=-66
+d=69.119
+e=-3 
+f=50
+
+#if else statements
+#if a<6 and b>3:
+#  message = 'amazing' 
+#  print(message)
+#elif b<20:
+#  message = 'ultra'
+#  print(message)
+#elif a>60:
+#  message = 'zuper'
+#  print(message)
+#else
+#  message = 'oh nawrrrrr'
+#  print(message)
+
+#pos or neg if else statments
+#num = round(f,2)
+if num < 0:
+	print('we gotta negative boyssss')
+elif num > 0: 
+	print('we gotta positive boyssss')
+elif num == 0:
+	print('we got zero boyssssssssss')
+
+#more nested scripts
+if num < 50:
+	print('it is an even number that is smaller than 50')
+elif num / 2:
+ 	print('probably even')
+elif num > 50 and num / 3:
+	print('it is larger than 50 and divisible by 3')
+ 
