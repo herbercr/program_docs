eda5c85e882d4a4667b304beadd7f147ab6cc461
student <student@info-15.cshl.edu>
2022-10-17 21:14:44 -0400

    update for NGS and genome assembly

--- NGS/NGS_workshop.py
+++ NGS/NGS_workshop.py
@@ -7,12 +7,15 @@ for line in fasta:
 	line = line.rstrip()
 	if line.startswith(">"):
 		gene = line.lstrip()
-		print(gene)
+#		print(gene)
 	else:
-		print(len(line)) #prints line lengths but not the total lengths in the fasta
+#		print(len(line)) #prints line lengths but not the total lengths in the fasta
 		seqn = line
 		fasta_ecoli[gene] = seqn #dictionary is built 
-print(fasta_ecoli.items()) #prints dictionary key; values
+#print(fasta_ecoli.items()) #prints dictionary key; values
+#print(len(seqn))	#prints 1? 
+#print(fasta_ecoli.values()) #prints only a few bases for each gene?
+
 
 
 
